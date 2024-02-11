6dc96f9d466df93178f40fe790487711dacecbf5
GabrielHS <gabrielhenri0012@gmail.com>
2024-02-11 18:47:04 -0300

    feat: find users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/database/PrismaService';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService) {}
+
+    async create(data: Prisma.UserCreateInput) {
+        return this.prisma.user.create({ data });
+    }
+
+    async findAll() {
+        return this.prisma.user.findMany();
+    }
+}
