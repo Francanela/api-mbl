f9f625a7d66676e2b67c0fd1da9b7d3857727992
Alexandre Francanela <alexandre.francanela@avalara.com>
2024-02-12 14:12:57 -0300

    application

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { AddressController } from './address.controller';
 import { PrismaService } from 'src/database/PrismaService';
-import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [AddressController],
