f569b72d6dad181ce85da8bc982a95bc48f89975
Daniel Bastos <daniel.bastos@ef5.com.br>
2024-02-12 01:39:27 -0300

    endpoints e hasheamento de senha

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,9 +9,18 @@ import { AddressModule } from './address/address.module';
 import { PaymentCardsModule } from './payment-cards/payment-cards.module';
 
 @Module({
-  imports: [UsersModule, ApplicationsModule, PhonesModule, PaymentCardsModule, AddressModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    ApplicationsModule,
+    PhonesModule,
+    PaymentCardsModule,
+    AddressModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 
-export class AppModule {}
+export class AppModule { }
