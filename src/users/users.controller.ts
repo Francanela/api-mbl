527912931e804e1727ca113075e9aa0fa1d8e6eb
Daniel Bastos <daniel.bastos@ef5.com.br>
2024-02-11 23:34:20 -0300

    Removendo console.log

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,7 +13,6 @@ export class UsersController {
 
     @Post()
     async create(@Body() createUserDto: CreateUserDto) {
-      console.log(createUserDto);
       return this.usersService.create(createUserDto);
     }
   }
