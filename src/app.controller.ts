c2dec411a66ab7ed2c9b5222c0e01e66c819219f
Alexandre Francanela <alexandre.francanela@avalara.com>
2024-02-12 23:09:49 -0300

    MERGE

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger'; 
+import { get } from 'http';
 
 @Controller()
 @ApiTags()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get()
+  HelloWord() {
+    return "Hello guys"
+  }
   
 }
