ab3a4d595456cd2c5ce08b44d01ef3e61b1295c6
ThiagoBLGalvao <thiagoblgalvao@hotmail.com>
2024-02-12 21:44:31 -0300

    fixed identation

--- src/payment-cards/payment-cards.service.ts
+++ src/payment-cards/payment-cards.service.ts
@@ -9,78 +9,78 @@ import { CreateLogDto } from 'src/log/dto/create-log.dto';
 
 @Injectable()
 export class PaymentCardsService {
-    constructor(
-      private prisma: PrismaService,
-      private readonly logService: LogService,
-      private readonly logConst: LogConsts
-    ) {}
-    
-    async create(createPaymentCardDto: CreatePaymentCardDto) {
-      const createdCard = await this.prisma.paymentCard.create({
-        data: createPaymentCardDto
-      });
+  constructor(
+    private prisma: PrismaService,
+    private readonly logService: LogService,
+    private readonly logConst: LogConsts
+  ) { }
 
-      this.logService.log(
-        new CreateLogDto(
+  async create(createPaymentCardDto: CreatePaymentCardDto) {
+    const createdCard = await this.prisma.paymentCard.create({
+      data: createPaymentCardDto
+    });
+
+    this.logService.log(
+      new CreateLogDto(
         1,
         this.logConst.createOperation,
         this.logConst.paymentCardEntity,
         createdCard.id.toString()
-        )
       )
-      return createdCard;
-    }
+    )
+    return createdCard;
+  }
 
-    findCard(id: number) {
-      return this.prisma.paymentCard.findUnique({
-        where:{
-          id: id,
-        }
-      })    
-    }
+  findCard(id: number) {
+    return this.prisma.paymentCard.findUnique({
+      where: {
+        id: id,
+      }
+    })
+  }
 
-    findUserCards(userId: number) {
-      return this.prisma.paymentCard.findMany({
-        where:{
-          user_id: userId,
-          deleted_at: null
-        }
-      })    
-    }
+  findUserCards(userId: number) {
+    return this.prisma.paymentCard.findMany({
+      where: {
+        user_id: userId,
+        deleted_at: null
+      }
+    })
+  }
 
-    async update(id: number, updatePaymentCardDto: UpdatePaymentCardDto) {
-      const originalPaymentCard = ( await this.findCard(id)) 
+  async update(id: number, updatePaymentCardDto: UpdatePaymentCardDto) {
+    const originalPaymentCard = (await this.findCard(id))
 
-      this.logService.log(
-        new CreateLogDto(
-          1,
-          this.logConst.updateOperation,
-          this.logConst.paymentCardEntity,
-          JSON.stringify(originalPaymentCard)
-        )
+    this.logService.log(
+      new CreateLogDto(
+        1,
+        this.logConst.updateOperation,
+        this.logConst.paymentCardEntity,
+        JSON.stringify(originalPaymentCard)
       )
+    )
 
-      return this.prisma.paymentCard.update({
-        where: {id: id},
-        data: updatePaymentCardDto
-      })
-    }
+    return this.prisma.paymentCard.update({
+      where: { id: id },
+      data: updatePaymentCardDto
+    })
+  }
 
-    remove(id: number) {
-      const removedCard = this.prisma.paymentCard.update({
-        where: {id: id},
-        data: {deleted_at: new Date()}
-      })
+  remove(id: number) {
+    const removedCard = this.prisma.paymentCard.update({
+      where: { id: id },
+      data: { deleted_at: new Date() }
+    })
 
-      this.logService.log(
-        new CreateLogDto(
-          1,
-          this.logConst.deleteOperation,
-          this.logConst.paymentCardEntity,
-          id.toString()
-        )
+    this.logService.log(
+      new CreateLogDto(
+        1,
+        this.logConst.deleteOperation,
+        this.logConst.paymentCardEntity,
+        id.toString()
       )
+    )
 
-      return removedCard
-    }
+    return removedCard
+  }
 }
